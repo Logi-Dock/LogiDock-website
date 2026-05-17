039257f5a1c74309f91bba879e0654af42e22ee3
Lucas-S-Gama <lucas.gama@sptech.school>
2026-05-17 14:38:12 -0300

    Login integrado na web-data-viz

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,4 +1,13 @@
-var database = require("../database/config")
+var database = require("../database/config");
+
+function autenticar(email_user, senha_user) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email_user, senha_user)
+    var instrucaoSql = `
+        SELECT id_usuario, nome_user, email_user FROM usuario WHERE email_user = '${email_user}' AND senha_user = '${senha_user}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 
 
 function cadastrarUsuario(nome_user, email_user, senha_user) {
@@ -22,6 +31,7 @@ function PegarIdUsuario(email_user) {
 }
 
 module.exports = {
+    autenticar,
     cadastrarUsuario,
     PegarIdUsuario
 }
