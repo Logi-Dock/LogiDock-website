87b8137749212686139c65bed908ad8cf8eeb8e3
pedrocosta16 <pedrocpereira16@gmail.com>
2026-05-30 19:04:11 -0300

    Adicionando estrutura para conexão da dashboard com o banco de dados

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var empresaRouter = require("./src/routes/empresas");
 var nivelAcessoRouter = require("./src/routes/nivelAcesso.js");
+var dashboardRouter = require("./src/routes/dashboard.js");
 // var bobRouter = require("./src/routes/bob.js"); // BOBIA
 
 app.use(express.json());
@@ -40,6 +41,7 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/empresas", empresaRouter);
 app.use("/nivelAcesso", nivelAcessoRouter);
+app.use("/dashboard", dashboardRouter);
 // app.use("/bob", bobRouter); // BOBIA
 
 app.listen(PORTA_APP, function () {
